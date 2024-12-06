004e6d75c122bfeafcc3ea3291f8e8bab9d3a3ca
“chandan” <chandanabbupur@gmail.com>
2024-12-06 11:36:21 +0530

    done

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a CI/CD demo!"
+    return "Hello, Worldd! This is a CI/CD demo!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
